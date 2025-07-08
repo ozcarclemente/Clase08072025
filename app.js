01b03f6384c19a94e5f6d97740647031fe50a51b
Ozcar Clemente <163335710+ozcarclemente@users.noreply.github.com>
2025-07-08 09:42:17 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const moment = require("moment");
+let i = 5;
+let j = 10;
+console.log("Iniciando");
+console.log("La suma es " + (i + j));
+
+let fecha = moment().format("LL");
+console.log("Y la fecha es:", fecha);
+
+
